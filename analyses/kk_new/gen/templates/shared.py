93a10dc49048caf38381cf7a62d55d29d988da1d
spark4 <xuebaxuan@gmail.com>
2026-06-03 21:51:53 +0800

    finish workdir config

---
+++ analyses/kk_new/gen/templates/shared.py
@@ -0,0 +1,97 @@
+
+# SECTION: COMMON_UTILITIES
+import copy
+import json
+import logging
+import logging.config
+import os
+# must set before importing jax
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+import time
+import numpy as onp
+from functools import partial
+from scipy.optimize import minimize
+
+import jax.numpy as np
+from jax import device_put, grad, jit, vmap, jvp
+from jax import config
+
+
+# SECTION: PATH_CONFIG
+import sys
+foo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(foo_path)
+sys.path.append(foo_path)
+
+
+# SECTION: DPLEX_FUNCTIONS
+def dplex_deinsum(subscript, aa, bb):
+    real = np.einsum(subscript, aa[0], bb[0]) - np.einsum(subscript, aa[1], bb[1])
+    imag = np.einsum(subscript, aa[0], bb[1]) + np.einsum(subscript, aa[1], bb[0])
+    return np.stack([real, imag], axis=0)
+
+def dplex_deinsum_ord(subscript, aa, bb):
+    real = np.einsum(subscript, aa, bb[0])
+    imag = np.einsum(subscript, aa, bb[1])
+    return np.stack([real, imag], axis=0)
+
+def dplex_dabs(aa):
+    return aa[0]**2 + aa[1]**2 # 因为是纵向叠加所以aa[0]是纵向上第一个数组
+
+def dplex_dtomine(aa):
+    return np.stack([np.real(aa), np.imag(aa)], axis=0) # 组合而成的数组 axis=0 的第一个数组为实部，第二个数组为虚部
+
+def dplex_dconstruct(aa, bb):
+    return np.stack([aa, bb], axis=0) # 纵向叠加数组 第一维为实部，第二维为虚部
+
+def dplex_ddivide(a, bb):
+    real = a * bb[0] / dplex_dabs(bb)
+    imag = -a * bb[1] / dplex_dabs(bb)
+    return np.stack([real, imag], axis=0)
+
+# SECTION: PHYSICS_FUNCTIONS
+def BW(m_,w_,Sbc):
+    l = (Sbc.shape)[0]
+    temp = dplex_dconstruct(m_*m_ - Sbc,  -m_*w_*np.ones(l))
+    return dplex_ddivide(1.0, temp)
+
+def BW_relativity(m_,w_,Sbc):
+    gamma=np.sqrt(m_*m_*(m_*m_+w_*w_))
+    k = np.sqrt(2*np.sqrt(2)*m_*np.abs(w_)*gamma/np.pi/np.sqrt(m_*m_+gamma))
+    l = (Sbc.shape)[0]
+    temp = dplex_dconstruct(m_*m_ - Sbc,  -m_*w_*np.ones(l))
+    return dplex_ddivide(k, temp)
+
+def flatte980(m_,g_pipi,rg,Sbc):
+    g_kk = rg * g_pipi
+    m_k = 0.493677
+    m_pi = 0.13957061
+    rho_kk = np.sqrt(np.abs(1 - 4*m_k*m_k / Sbc))
+    rho_pipi = np.sqrt(np.abs(1 - 4*m_pi*m_pi / Sbc))
+    tmp_A = dplex_dconstruct(m_**2 - Sbc, -1*(g_pipi*rho_pipi + g_kk*rho_kk))
+    return dplex_ddivide(1.0, tmp_A)
+
+def flatte1270(m_,w_,Sbc):
+    rm = m_ * m_
+    gr = m_ * w_
+    q2r = 0.25 * rm - 0.0194792
+    b2r = q2r * (q2r + 0.1825) + 0.033306
+    g11270 = gr * b2r / np.power(q2r,2.5)
+    q2 = 0.25 * Sbc - 0.0194792
+    b2 = q2 * (q2 + 0.1825) + 0.033306
+    g1 = g11270 * np.power(q2,2.5) / b2
+    tmp = dplex_dconstruct(Sbc - rm, g1)
+    return dplex_ddivide(gr, tmp)
+
+def flatte500(m_,b1,b2,b3,b4,b5,Sbc):
+    m2 = m_*m_
+    rp = 0.139556995
+    mpi2d2 = 0.009739946882
+    cro1 = np.sqrt(np.abs((Sbc-(2*rp)**2)*Sbc))/Sbc
+    cro2 = np.sqrt(np.abs((m2-(2*rp)**2)*m2))/m2
+    pip1 = np.sqrt(np.abs(1.0 - 0.3116765584/Sbc))/(1.0+np.exp(9.8-3.5*Sbc))
+    pip2 = np.sqrt(np.abs(1.0 - 0.3116765584/m2))/(1.0+np.exp(9.8-3.5*m2)) # ?
+    cgam1 = m_*(b1+b2*Sbc)*(Sbc-mpi2d2)/(m2-mpi2d2)*np.exp(-(Sbc-m2)/b3)*cro1/cro2
+    cgam2 = m_*b4*pip1/pip2
+    tmp = dplex_dconstruct(m2-Sbc, -b5*(cgam1+cgam2))
+    return dplex_ddivide(1.0, tmp)
